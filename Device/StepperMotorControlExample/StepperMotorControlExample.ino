c58b709ce20222c0aaa92e3133f1a5e7e03240ee
bakasura980 <kariop980>
2017-02-24 07:38:44 +0300

    Add examples

---
+++ Device/StepperMotorControlExample/StepperMotorControlExample.ino
@@ -0,0 +1,28 @@
+const int stepPin = 3; 
+const int dirPin = 4; 
+ 
+void setup() {
+  pinMode(stepPin,OUTPUT); 
+  pinMode(dirPin,OUTPUT);
+}
+void loop() {
+  digitalWrite(dirPin,HIGH);
+ 
+  for(int x = 0; x < 200; x++) {
+    digitalWrite(stepPin,HIGH); 
+    delayMicroseconds(500); 
+    digitalWrite(stepPin,LOW); 
+    delayMicroseconds(500); 
+  }
+  delay(1000); 
+  
+  digitalWrite(dirPin,LOW); 
+  
+  for(int x = 0; x < 400; x++) {
+    digitalWrite(stepPin,HIGH);
+    delayMicroseconds(500);
+    digitalWrite(stepPin,LOW);
+    delayMicroseconds(500);
+  }
+  delay(1000);
+}
